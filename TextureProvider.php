73be5be4fbc04414c0223be18b67ac0825ba37fa
microwin7 <usa.microwin8@gmail.com>
2021-08-02 08:18:33 +0300

    Update TextureProvider.php

--- TextureProvider.php
+++ TextureProvider.php
@@ -11,11 +11,11 @@ class Constants
 {
     const SKIN_PATH = "./minecraft-auth/skins/"; // Сюда вписать путь до skins/
     const CLOAK_PATH = "./minecraft-auth/cloaks/"; // Сюда вписать путь до cloaks/
-    const SKIN_URL = "http://127.0.0.1/minecraft-auth/skins/%login%.png";
-    const CLOAK_URL = "http://127.0.0.1/minecraft-auth/cloaks/%login%.png";
+    const SKIN_URL = "https://example.com/minecraft-auth/skins/%login%.png";
+    const CLOAK_URL = "https://example.com/minecraft-auth/cloaks/%login%.png";
     const REGEX_USERNAME = "\w{1,16}$";
     const REGEX_UUID = "[a-f0-9]{8}\-[a-f0-9]{4}\-4[a-f0-9]{3}\-(8|9|a|b)[a-f0-9]{3}\-[a-f0-9]{12}";
-
+    
     public static function getSkinURL($login)
     {
         return str_replace('%login%', $login, self::SKIN_URL);
@@ -43,7 +43,7 @@ class Msg
 }
 if (strnatcmp(phpversion(), '5.6') >= 0) {
     $msg = new Msg();
-    if(!regex_valid($login)) $msg->exit();
+    regex_valid($login) ?: $msg->exit();
     $msg->add('skin', check_skin($login));
     $msg->add('cloak', check_cloak($login));
     $msg->exit();
@@ -59,14 +59,14 @@ function check_skin($login)
         $size = getimagesize($loadskin);
         $fraction = $size[0] / 8;
         $image = imagecreatefrompng($loadskin);
-        $x1 = $fraction * 6.75;
-        $y1 = $fraction * 2.5;
-        $rgba = imagecolorat($image, $x1, $y1);
+        $x = $fraction * 6.75;
+        $y = $fraction * 2.5;
+        $rgba = imagecolorat($image, $x, $y);
         if (($rgba & 0x7F000000) >> 24) {
             $msg['metadata'] = array('model' => 'slim');
         }
+        return $msg;
     }
-    return $msg;
 }
 function check_cloak($login)
 {
@@ -76,15 +76,14 @@ function check_cloak($login)
             'url' => Constants::getCloakURL($login),
             'digest' => Constants::getBase64Encode_MD5(file_get_contents($loadskin))
         );
+        return $msg;
     }
-    return $msg;
 }
 function regex_valid($var)
 {
     if (!is_null($var) && (preg_match("/^" . Constants::REGEX_USERNAME . "/", $var, $varR) ||
         preg_match("/" . Constants::REGEX_UUID . "/", $var, $varR)))
         return true;
-    else return false;
 }
 function ci_find_file($filename)
 {
